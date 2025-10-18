cc8b5ef342a84a937b159d8fe496ffde3522b117
AndreiFoxes10 <71722314+AndreiFoxes10@users.noreply.github.com>
2025-10-18 17:16:15 +0300

    Add files via upload

--- package/Wisdarmo Blue Grass Lite - Chromium B.E. Edition/js/content.js
+++ package/Wisdarmo Blue Grass Lite - Chromium B.E. Edition/js/content.js
@@ -19,6 +19,8 @@ const message3 = "Search engine removed!"
 
 const message4 = "Search engine is not or already removed. :/"
 
+const message5 = "This website is forbidden!"
+
 const wbgls1 = chrome.storage.local
 
 // 1.1 YouTube(Desktop version) (W.I.P.)
@@ -47,6 +49,32 @@ const ytInnerHTML1 = `
 	</div>`
 	
 // 1.2 YouTube (Mobile versin) (W.I.P.)
+
+// 4. Instagram
+const igContent1 = document.querySelector("section.x78zum5.xdt5ytf.x1iyjqo2.xg6iff7")
+
+const igInnerHTML1 = `
+	<main class="xvbhtw8 x78zum5 xdt5ytf x1iyjqo2 xl56j7k" role="main">
+		<div style="max-width: 100%;">
+			<div class="x9f619 x1n2onr6 x1ja2u2z x78zum5 xdt5ytf x2lah0s x193iq5w x6s0dn4 xf7dkkf xv54qhq">
+				<div class="html-div xdj266r x14z9mp xat24cr x1lziwak xexx8yu xyri2b x18d9i69 x1c1uobl x9f619 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1">
+					<svg aria-label="" class="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="96" role="img" viewBox="0 0 96 96" width="96">
+					<title></title>
+					<circle cx="48" cy="48" fill="none" r="47" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle>
+					<path d="M60.931 70.001H35.065a5.036 5.036 0 0 1-5.068-5.004V46.005A5.036 5.036 0 0 1 35.065 41H60.93a5.035 5.035 0 0 1 5.066 5.004v18.992A5.035 5.035 0 0 1 60.93 70ZM37.999 39.996v-6.998a10 10 0 0 1 20 0v6.998" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path></svg>
+				</div>
+				<span style="--x---base-line-clamp-line-height: 30px; --x-lineHeight: 30px;" dir="auto" class="x1lliihq x1plvlek xryxfnj x1n2onr6 xyejjpt x15dsfln x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye x133cpev x1s688f x5n08af x2b8uid x4zkp8e x41vudc x10wh9bi xpm28yp x8viiok x1o7cslx x1cnzs8">Instagram is blocked!</span>
+				<div class="html-div x14z9mp x1lziwak xexx8yu xyri2b x18d9i69 x1c1uobl x9f619 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw x1tfhste xieb3on x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1">
+					<span class="x1lliihq x1plvlek xryxfnj x1n2onr6 xyejjpt x15dsfln x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xl565be xo1l8bm x5n08af x2b8uid x1tu3fi x3x7a5m x10wh9bi xpm28yp x8viiok x1o7cslx" style="--x---base-line-clamp-line-height: 20px; --x-lineHeight: 20px;" dir="auto">
+						We apologize for that, published contents are hidden like YouTube Unlisted for these reason.
+						<br>
+						If you want to see the posts, then you can check on URL address, Links, Browser History, and/or Bookmark to view it again.
+					</span>
+				</div>
+			</div>
+		</div>
+	</main>
+`
 	
 // 7 Sketchers United
 const suSearch1 = document.querySelector("input#searchInput")
@@ -126,210 +154,394 @@ const ngInnerHTML1 = `
 	</div>
 `
 
-// Second
-// 1 YouTube
-wbgls1.get(["ls_turnoff_yt1"]).then((result) => {
-	let turnoff1 = result.ls_turnoff_yt1;
-	if (turnoff1 = true) {
-		// Home Page(Recommendations)
-		if (site1.includes("youtube.com")) {
-			ytRemoveSearch1();
-			// Contents
-			
-			if (ytContents2){
-				ytContents2.innerHTML = ytInnerHTML1; 
-				//ytContents1.remove();
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
-			}
-		}
-	}
-})
+// Access restriction mode
+const rmHead1 = document.head
 
-// 7.Sketchers United
-wbgls1.get(["ls_turnoff_su1"]).then((result) => {
-	let turnoff7 = result.ls_turnoff_su1;
-	if (turnoff7 == true) {
-		// Main
-		if (site1.includes("sketchersunited.org")) {
-			if (suSearch1){
-				suSearch1.remove();
-				console.log(title1 + message3);
-				} else {
-				console.log(title1 + message4);
-			}
-		}
-		
-		// Contents
-		if (site2.includes("sketchersunited.org/posts")) {
-			if (suContent1){
-				suContent1.innerHTML = suInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
-			}
-		}
-		
-		
-		// Channels
-		if (site2.includes("skechtersunited.org/channels/") & Math.random()) {
-			if (suContent2){
-				suContent2.innerHTML = suInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
-			}
-		}
-		
-		// Users 
-		if (site2.includes("skechtersunited.org/users/") + Math.random()) {
-			if (suContent2){
-				suContent2.innerHTML = suInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
-			}
-		}
-		
-		// Tags
-		if (site2.includes("skechtersunited.org/tags/") + Math.random()) {
-			if (suContent2){
-				suContent2.innerHTML = suInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
-			}
-		}
-		/* (unused and not working)
-		// Collabs
-		if (site2.includes("sketchers.org/collabs/") + Math.random()) {
-			let su_openCollabs1 = false;
-			su_openCollabs1 = true;
-			if (suContent1){
-				if (su_openCollabs1 == true){
-					console.log(title1 + "Collab, huh?");
+const rmBody1 = document.body
+
+const rm_innerhtml1 = `
+	<div class="container">
+		<h1>This "` + site1 + `" access is restricted!</h1>
+		We apologize for that, this Website/Social Media access is restricted for these reason.<br><br>
+		Please, go outside and touch some grass.
+		<br>
+		<br>
+		<img src="` + chrome.runtime.getURL('img/In-Case-Of-Emergency-Turn-Off-The-Internet-Social-Media-Logo2.png') + `" width="200"/>
+	</div>
+`
+
+const rm_innerhtml2 =`
+	<title>Restricted access</title>
+	<link rel="stylesheet" href="` + chrome.runtime.getURL('css/common.css') +`">
+	<link rel="stylesheet" href="` + chrome.runtime.getURL('css/restrictedpagestyle.css') +`">
+`
+
+// Second
+wbgls1.get(["ls_wbgswitch1"]).then((result) => {
+	let srm1 = result.ls_wbgswitch1;
+	// Regular settings
+	if (srm1 == 0) {
+		// 1 YouTube
+		wbgls1.get(["ls_turnoff_yt1"]).then((result) => {
+			let turnoff1 = result.ls_turnoff_yt1;
+			if (turnoff1 == true) {
+				// Home Page(Recommendations)
+				if (site1.includes("youtube.com")) {
+					ytRemoveSearch1();
+					// Contents
+					
+					if (ytContents2){
+						ytContents2.innerHTML = ytInnerHTML1; 
+						//ytContents1.remove();
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
 				}
 			}
-		}
-		*/
-	}
-})
-
-// 8 Newgrounds
-wbgls1.get("ls_turnoff_ng1").then((result) => {
-	let turnoff8 = result.ls_turnoff_ng1;
-	if (turnoff8 == true) {
-		// Home/Main
-		if (site1.includes("newgrounds.com")) {
-			if (ngSearch1) {
-				ngSearch1.remove();
-				ngFooter1.remove();
-				console.log(title1 + message3)
-			} else {
-				console.log(title1 + message2);
-			}
+		})
+		
+		// 4 Instagram
+		wbgls1.get(["ls_turnoff_ig1"]).then((result) => {
+			let turnoff4 = result.ls_turnoff_ng1;
+			if (turnoff4 == true) {
+				// Main
 			
-			if (ngContents2) {
-				ngContents2.remove();
-				ngContents3.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+				// Home
+				if (site2.includes("instagram.com")) {
+					igContent1.innerHTML = igContent1;
+					console.log(title1 + message3);
+				} else {
+					console.log(title1 + message4);
+				}
 			}
-		}
+		}) 
 		
-		// Movies
-		if (site2.includes("www.newgrounds.com/movies")) {
-			if (ngContents4) {
-				ngContents4.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+		// 7 Sketchers United
+		wbgls1.get(["ls_turnoff_su1"]).then((result) => {
+			let turnoff7 = result.ls_turnoff_su1;
+			if (turnoff7 == true) {
+				// Main
+				if (site1.includes("sketchersunited.org")) {
+					if (suSearch1){
+						suSearch1.remove();
+						console.log(title1 + message3);
+					} else {
+						console.log(title1 + message4);
+					}
+				}
+			
+				// Contents
+				if (site2.includes("sketchersunited.org/posts")) {
+					if (suContent1){
+						suContent1.innerHTML = suInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				
+				// Channels
+				if (site2.includes("skechtersunited.org/channels/") & Math.random()) {
+					if (suContent2){
+						suContent2.innerHTML = suInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Users 
+				if (site2.includes("skechtersunited.org/users/") + Math.random()) {
+					if (suContent2){
+						suContent2.innerHTML = suInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Tags
+				if (site2.includes("skechtersunited.org/tags/") + Math.random()) {
+					if (suContent2){
+						suContent2.innerHTML = suInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				/* (unused and not working)
+				// Collabs
+				if (site2.includes("sketchers.org/collabs/") + Math.random()) {
+					let su_openCollabs1 = false;
+					su_openCollabs1 = true;
+					if (suContent1){
+						if (su_openCollabs1 == true){
+							console.log(title1 + "Collab, huh?");
+						}
+					}
+				}
+				*/
 			}
-		}
+		})
+
+		// 8 Newgrounds
+		wbgls1.get("ls_turnoff_ng1").then((result) => {
+			let turnoff8 = result.ls_turnoff_ng1;
+			if (turnoff8 == true) {
+				// Home/Main
+				if (site1.includes("newgrounds.com")) {
+					if (ngSearch1) {
+						ngSearch1.remove();
+						ngFooter1.remove();
+						console.log(title1 + message3)
+					} else {
+						console.log(title1 + message2);
+					}
+					
+					if (ngContents2) {
+						ngContents2.remove();
+						ngContents3.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Movies
+				if (site2.includes("www.newgrounds.com/movies")) {
+					if (ngContents4) {
+						ngContents4.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Games
+				if (site2.includes("www.newgrounds.com/games")) {
+					if (ngContents4) {
+						ngContents4.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Audio
+				if (site2.includes("www.newgrounds.com/audio")) {
+					if (ngContents4) {
+						ngContents4.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Art
+				if (site2.includes("www.newgrounds.com/art")) {
+					if (ngContents4) {
+						ngContents4.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Portal
+				if (site2.includes("www.newgrounds.com/portal")) {
+					if (ngContents5) {
+						ngContents5.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Community
+				if (site2.includes("www.newgrounds.com/community")) {
+					if (ngContents1) {
+						ngContents1.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Feed
+				if (site2.includes("www.newgrounds.com/social")) {
+					if (ngContents6) {
+						ngContents6.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				// Search
+				if (site2.includes("www.newgrounds.com/search/")) {
+					if (ngSearch2) {
+						ngSearch2.innerHTML = ngInnerHTML1;
+						console.log(title1 + message3);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				
+				/*
+				// Users
+				if (site2.includes("https://" + Math.random(Math.floor()) + ".newgrounds.com")){
+					if (ngContents3) {
+						ngContents7.remove();
+						ngContents3.innerHTML = ngInnerHTML1;
+						console.log(title1 + message1);
+					} else {
+						console.log(title1 + message2);
+					}
+				}
+				*/
+			}
+		})
+		// Advanced settings
+		// Access restriction mode
+		// 1 YouTube
+		wbgls1.get(["ls_turnoff_yt2"]).then((result) => {
+			let rm_turnoff1 = result.ls_turnoff_yt2;
+			if (rm_turnoff1 == true) {
+				if (site1.includes("youtube.com")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
+			}
+		})
 		
-		// Games
-		if (site2.includes("www.newgrounds.com/games")) {
-			if (ngContents4) {
-				ngContents4.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+		// 2 Twitter/X
+		wbgls1.get(["ls_turnoff_twitter2"]).then((result) => {
+			let rm_turnoff2 = result.ls_turnoff_twitter2;
+			if (rm_turnoff2 == true) {
+				if (site1.includes("x.com")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
 			}
-		}
+		})
 		
-		// Audio
-		if (site2.includes("www.newgrounds.com/audio")) {
-			if (ngContents4) {
-				ngContents4.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+		// 3 Facebook
+		wbgls1.get(["ls_turnoff_fb2"]).then((result) => {
+			let rm_turnoff3 = result.ls_turnoff_fb2;
+			if (rm_turnoff3 == true) {
+				if (site1.includes("facebook.com")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
 			}
-		}
+		})
 		
-		// Art
-		if (site2.includes("www.newgrounds.com/art")) {
-			if (ngContents4) {
-				ngContents4.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+		// 4 Instagram
+		wbgls1.get(["ls_turnoff_ig2"]).then((result) => {
+			let rm_turnoff4 = result.ls_turnoff_ig2;
+			if (rm_turnoff4 == true) {
+				if (site1.includes("instagram.com")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
 			}
-		}
+		})
 		
-		// Portal
-		if (site2.includes("www.newgrounds.com/portal")) {
-			if (ngContents5) {
-				ngContents5.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+		// 5 TikTok
+		wbgls1.get(["ls_turnoff_tiktok2"]).then((result) => {
+			let rm_turnoff5 = result.ls_turnoff_tiktok2;
+			if (rm_turnoff5 == true) {
+				if (site1.includes("tiktok.com")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
 			}
-		}
+		})
 		
-		// Community
-		if (site2.includes("www.newgrounds.com/community")) {
-			if (ngContents1) {
-				ngContents1.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+		// 6 DeviantArt
+		wbgls1.get(["ls_turnoff_da2"]).then((result) => {
+			let rm_turnoff6 = result.ls_turnoff_da2;
+			if (rm_turnoff6 == true) {
+				if (site1.includes("deviantart.com")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
 			}
-		}
+		})
 		
-		// Feed
-		if (site2.includes("www.newgrounds.com/social")) {
-			if (ngContents6) {
-				ngContents6.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+		// 7 Sketchers United
+		wbgls1.get(["ls_turnoff_su2"]).then((result) => {
+			let rm_turnoff7 = result.ls_turnoff_su2;
+			if (rm_turnoff7 == true) {
+				if (site1.includes("sketchersunited.org")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
 			}
-		}
+		})
 		
-		// Search
-		if (site2.includes("www.newgrounds.com/search/")) {
-			if (ngSearch2) {
-				ngSearch2.innerHTML = ngInnerHTML1;
-				console.log(title1 + message3);
-			} else {
-				console.log(title1 + message2);
+		// 8 Newgrounds
+		wbgls1.get(["ls_turnoff_ng2"]).then((result) => {
+			let rm_turnoff8 = result.ls_turnoff_ng2;
+			if (rm_turnoff8 == true) {
+				if (site1.includes("newgrounds.com")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
 			}
-		}
+		})
 		
-		/*
-		 // Users
-		 if (site2.includes("https://" + Math.random(Math.floor()) + ".newgrounds.com")){
-			 if (ngContents3) {
-				ngContents7.remove();
-				ngContents3.innerHTML = ngInnerHTML1;
-				console.log(title1 + message1);
-			} else {
-				console.log(title1 + message2);
+		// 9 Discord
+		wbgls1.get(["ls_turnoff_discord2"]).then((result) => {
+			let rm_turnoff9 = result.ls_turnoff_discord2;
+			if (rm_turnoff9 == true) {
+				if (site1.includes("discord.com")) {
+					if (rmBody1) {
+						rmBody1.innerHTML = rm_innerhtml1;
+						rmHead1.innerHTML = rm_innerhtml2;
+					} else {
+						console.log(title1 + "Restriction not working. :(");
+					}
+				}
 			}
-		 }
-		*/
+		})
+	} else if (srm1 == 1) {
+		// Super access restriction mode (W.I.P.)
+		console.log(title1 + message5);
+		
 	}
 })
--- package/Wisdarmo Blue Grass Lite - Chromium B.E. Edition/js/options.js
+++ package/Wisdarmo Blue Grass Lite - Chromium B.E. Edition/js/options.js
@@ -10,23 +10,52 @@ wbgls1.get(["ls_wbgstarter1"]).then((result) => {
 	let wbgstarter1 = result.ls_wbgstarter1;
 	if (wbgstarter1 != true) {
 		console.log(title1 + "LS starter undetected, LS's are now added.");
+		// Regular option
 		wbgls1.set({ "ls_wbgstarter1": true });
-		wbgls1.set({ "ls_turnoff_yt1": true });
+		wbgls1.set({ "ls_wbgswitch1": 0 });
+		wbgls1.set({ "ls_turnoff_yt1": false });
+		//wbgls1.set({ "ls_turnoff_ig1": true });
 		wbgls1.set({ "ls_turnoff_su1": true });
 		wbgls1.set({ "ls_turnoff_ng1": false });
+		
+		// Advanced option
+		wbgls1.set({ "ls_turnoff_yt2": false });
+		wbgls1.set({ "ls_turnoff_twitter2": false });
+		wbgls1.set({ "ls_turnoff_fb2": false });
+		wbgls1.set({ "ls_turnoff_ig2": false });
+		wbgls1.set({ "ls_turnoff_tiktok2": false });
+		wbgls1.set({ "ls_turnoff_da2": false });
+		wbgls1.set({ "ls_turnoff_su2": false });
+		wbgls1.set({ "ls_turnoff_ng2": false });
+		wbgls1.set({ "ls_turnoff_discord2": false });
 	} else {
 		console.log(title1 + "LS starter detected!");
 	}
 })
 
 resetsettings1.onclick = function() {
+	// Regular option
 	wbgls1.remove([ "ls_wbgstarter1" ]);
+	wbgls1.remove([ "ls_wbgswitch1" ]);
 	wbgls1.remove([ "ls_turnoff_yt1" ]);
+	//wbgls1.remove([ "ls_turnoff_ig1" ]);
 	wbgls1.remove([ "ls_turnoff_su1" ]);
 	wbgls1.remove([ "ls_turnoff_ng1" ]);
+	
+	// Advanced option
+	wbgls1.remove([ "ls_turnoff_yt2" ]);
+	wbgls1.remove([ "ls_turnoff_twitter2" ]);
+	wbgls1.remove([ "ls_turnoff_fb2" ]);
+	wbgls1.remove([ "ls_turnoff_ig2" ]);
+	wbgls1.remove([ "ls_turnoff_tiktok2" ]);
+	wbgls1.remove([ "ls_turnoff_da2" ]);
+	wbgls1.remove([ "ls_turnoff_su2" ]);
+	wbgls1.remove([ "ls_turnoff_ng2" ]);
+	wbgls1.remove([ "ls_turnoff_discord2" ]);
 	window.location.reload();
 }
 
+// Regular Options
 // First
 // 1 YouTube
 const checkbox1 = document.getElementById("turnoff_yt1");
@@ -48,14 +77,14 @@ let checkbox3 = document.getElementById("turnoff_fb1");
 wbgls1.get(["ls_turnoff_fb1"]).then((result) => {
 	checkbox3.checked = result.ls_turnoff_fb1;
 })
-
+*/
 // 4 Instragram
 let checkbox4 = document.getElementById("turnoff_ig1");
 
 wbgls1.get(["ls_turnoff_ig1"]).then((result) => {
 	checkbox4.checked = result.ls_turnoff_ig1;
 })
-
+/*
 // 5 TikTok
 let checkbox5 = document.getElementById("turnoff_tiktok1");
 
@@ -170,5 +199,188 @@ checkbox8.onclick = function() {
 	} else {
 		wbgls1.set({ "ls_turnoff_ng1": false });
 		console.log(title1 + "Newgrounds is on!");
+	}
+}
+
+// Advenced Options
+// Access restriction mode
+/*
+const rm_checkbox_starter = document.getElementById("turnon_rm1");
+
+wbgls1.get(["ls_wbgswitch1"]).then((result) => {
+	rm_checkbox_starter.checked = result.ls_wbgswitch1;
+})
+
+rm_checkbox_starter.onclick = function() {
+	if (rm_checkbox_starter.checked == true){
+		wbgls1.set({ "ls_wbgswitch1": 1 })
+		console.log(title1 + "Restriction mode is on!");
+	} else {
+		wbgls1.set({ "ls_wbgswitch1": 0 })
+		console.log(title1 + "Restriction mode is off!");
+	}
+}
+*/
+// Fisrt
+// 1 YouTube
+const rm_checkbox1 = document.getElementById("turnoff_yt2");
+
+wbgls1.get(["ls_turnoff_yt2"]).then((result) => {
+	rm_checkbox1.checked = result.ls_turnoff_yt2;
+})
+
+// 2 Twitter/X
+const rm_checkbox2 = document.getElementById("turnoff_twitter2");
+
+wbgls1.get(["ls_turnoff_twitter2"]).then((result) => {
+	rm_checkbox2.checked = result.ls_turnoff_twitter2;
+})
+
+// 3 Facebook
+const rm_checkbox3 = document.getElementById("turnoff_fb2");
+
+wbgls1.get(["ls_turnoff_fb2"]).then((result) => {
+	rm_checkbox3.checked = result.ls_turnoff_fb2;
+})
+
+// 4 Instagram
+const rm_checkbox4 = document.getElementById("turnoff_ig2");
+
+wbgls1.get(["ls_turnoff_ig2"]).then((result) => {
+	rm_checkbox4.checked = result.ls_turnoff_ig2;
+})
+
+// 5 TikTok
+const rm_checkbox5 = document.getElementById("turnoff_tiktok2");
+
+wbgls1.get(["ls_turnoff_tiktok2"]).then((result) => {
+	rm_checkbox5.checked = result.ls_turnoff_tiktok2;
+})
+
+// 6 DeviantArt
+const rm_checkbox6 = document.getElementById("turnoff_da2");
+
+wbgls1.get(["ls_turnoff_da2"]).then((result) => {
+	rm_checkbox6.checked = result.ls_turnoff_da2;
+})
+
+// 7 Sketchers United
+const rm_checkbox7 = document.getElementById("turnoff_su2");
+
+wbgls1.get(["ls_turnoff_su2"]).then((result) => {
+	rm_checkbox7.checked = result.ls_turnoff_su2;
+})
+
+// 8 Newgrounds
+const rm_checkbox8 = document.getElementById("turnoff_ng2");
+
+wbgls1.get(["ls_turnoff_ng2"]).then((result) => {
+	rm_checkbox8.checked = result.ls_turnoff_ng2;
+})
+
+// 9 Discord
+const rm_checkbox9 = document.getElementById("turnoff_discord2");
+
+wbgls1.get(["ls_turnoff_discord2"]).then((result) => {
+	rm_checkbox9.checked = result.ls_turnoff_discord2;
+})
+
+// Second
+// 1 YouTube
+rm_checkbox1.onclick = function() {
+	if (rm_checkbox1.checked == true){
+		wbgls1.set({ "ls_turnoff_yt2": true });
+		console.log(title1 + "YouTube access is off!");
+	} else {
+		wbgls1.set({ "ls_turnoff_yt2": false });
+		console.log(title1 + "YouTube access is on!");
+	}
+}
+
+// 2 Twitter/X
+rm_checkbox2.onclick = function() {
+	if (rm_checkbox2.checked == true){
+		wbgls1.set({ "ls_turnoff_twitter2": true });
+		console.log(title1 + "Twitter/X access is off!");
+	} else {
+		wbgls1.set({ "ls_turnoff_twitter2": false });
+		console.log(title1 + "Twitter/X access is on!");
+	}
+}
+
+// 3 Facebook
+rm_checkbox3.onclick = function() {
+	if (rm_checkbox3.checked == true){
+		wbgls1.set({ "ls_turnoff_fb2": true });
+		console.log(title1 + "Facebook access is off!");
+	} else {
+		wbgls1.set({ "ls_turnoff_fb2": false });
+		console.log(title1 + "Facebook access is on!");
+	}
+}
+
+// 4 Instagram
+rm_checkbox4.onclick = function() {
+	if (rm_checkbox4.checked == true){
+		wbgls1.set({ "ls_turnoff_ig2": true });
+		console.log(title1 + "Instagram access is off!");
+	} else {
+		wbgls1.set({ "ls_turnoff_ig2": false });
+		console.log(title1 + "Instagram access is on!");
+	}
+}
+
+// 5 TikTok
+rm_checkbox5.onclick = function() {
+	if (rm_checkbox5.checked == true){
+		wbgls1.set({ "ls_turnoff_tiktok2": true });
+		console.log(title1 + "TikTok access is off!");
+	} else {
+		wbgls1.set({ "ls_turnoff_tiktok2": false });
+		console.log(title1 + "TikTok access is on!");
+	}
+}
+
+// 6 DeviantArt
+rm_checkbox6.onclick = function() {
+	if (rm_checkbox6.checked == true){
+		wbgls1.set({ "ls_turnoff_da2": true });
+		console.log(title1 + "DeviantArt access is off!");
+	} else {
+		wbgls1.set({ "ls_turnoff_da2": false });
+		console.log(title1 + "DeviantArt access is on!");
+	}
+}
+
+// 7 Sketchers United
+rm_checkbox7.onclick = function() {
+	if (rm_checkbox7.checked == true){
+		wbgls1.set({ "ls_turnoff_su2": true });
+		console.log(title1 + "SU access is off!");
+	} else {
+		wbgls1.set({ "ls_turnoff_su2": false });
+		console.log(title1 + "SU access is on!");
+	}
+}
+
+// 8 Newgrounds
+rm_checkbox8.onclick = function() {
+	if (rm_checkbox8.checked == true){
+		wbgls1.set({ "ls_turnoff_ng2": true });
+		console.log(title1 + "Newgrounds access is off!(Please turn on, I beggin you. :'( )");
+	} else {
+		wbgls1.set({ "ls_turnoff_ng2": false });
+		console.log(title1 + "Newgrounds access is on! (Thank you, now it's much better :) )");
+	}
+}
+
+// 9 Discord
+rm_checkbox9.onclick = function() {
+	if (rm_checkbox9.checked == true){
+		wbgls1.set({ "ls_turnoff_discord2": true });
+		console.log(title1 + "Discord access is off!");
+	} else {
+		wbgls1.set({ "ls_turnoff_discord2": false });
+		console.log(title1 + "Discord access is on!");
 	}
 }
